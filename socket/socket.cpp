d8b3e830f3ffd74bbec054978dd24ed156eff0bd
Minh Kha <khatruong06@gmail.com>
2025-07-27 23:25:02 +1000

    Dev: Implementations for Socket

---
+++ socket/socket.cpp
@@ -0,0 +1,53 @@
+#include "socket.hpp"
+#include <iostream>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+// ---------------------------------- Constructor ----------------------------------->
+
+Socket::Socket(int port) : port(port)
+{
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1)
+    {
+        std::cerr << "Failed to create socket!" << std::endl;
+        exit(1);
+    }
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(this->port);
+}
+
+// ---------------------------------- Start listening ----------------------------------->
+
+void Socket::listenSocket()
+{
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+    {
+        std::cerr << "Bind failed!" << std::endl;
+        closeSocket();
+        exit(1);
+    }
+
+    if (listen(serverSocket, 3) == -1)
+    {
+        std::cerr << "Listen failed!" << std::endl;
+        closeSocket();
+        exit(1);
+    }
+}
+
+// ---------------------------------- Stop listening ----------------------------------->
+
+void Socket::closeSocket()
+{
+    close(this->serverSocket);
+}
+
+Socket::~Socket()
+{
+    closeSocket();
+}
