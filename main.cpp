68ada03ea43161443c0319a9045fe0ef1f10b2c6
Minh Kha <khatruong06@gmail.com>
2025-07-28 12:09:36 +1000

    Dev: Add more description

--- main.cpp
+++ main.cpp
@@ -6,14 +6,21 @@
 
 int main()
 {
+    // Initialize socket and server address
     Socket server(PORT);
+
+    // Bind socket
     server.listenSocket();
 
     std::cout << "Server is listening on port " << PORT << "..." << std::endl;
 
+    // Infinite loops for receiving incoming requests
     while (true)
     {
+        // server.getServerSocket() would return 3 as file descriptor for Socket.
+        // clientSocket may have a value of 4 as 0, 1, 2 has been there.
         int clientSocket = accept(server.getServerSocket(), nullptr, nullptr);
+
         if (clientSocket == -1)
         {
             std::cerr << "Failed to accept client connection!" << std::endl;
